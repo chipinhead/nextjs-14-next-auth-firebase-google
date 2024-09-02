5f03e012abcfe3f0aa90cbc46dc25530006565c3
Juni Samos <juni@Junis-MacBook-Pro.local>
2024-09-02 18:49:03 +0200

    Working Without Hacks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
-"use client";
-
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
+import SessionProvider from './SessionProvider';
 import React from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body>
-        <SessionProvider>{children}</SessionProvider>
+      <body style={{ backgroundColor: '#ccc' }}>
+        <header style={{ backgroundColor: '#004f76', padding: '1rem' }}>
+          <h1 style={{ color: 'white', margin: 0 }}>YOUR APP SUCKS</h1>
+        </header>
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
   );
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,34 +1,8 @@
-"use client";
+'use client';
+import { signIn } from 'next-auth/react';
 
-import { useEffect, useState } from "react";
-import { getProviders, signIn } from "next-auth/react";
-import React from 'react';
-
-const SignInPage = () => {
-  const [providers, setProviders] = useState(null);
-
-  useEffect(() => {
-    const fetchProviders = async () => {
-      const res = await getProviders();
-      setProviders(res);
-    };
-
-    fetchProviders();
-  }, []);
-
+export default function SignIn() {
   return (
-    <div>
-      <h1>Sign In</h1>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
-    </div>
-  );
-};
-
-export default SignInPage;
+    <button onClick={() => signIn('google')}>Sign In</button>
+  )
+}
