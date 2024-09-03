7fd4e8c962e75f42ca157bf2726040cbf27c7b5e
Juni Samos <juni@Junis-MacBook-Pro.local>
2024-09-03 12:49:44 +0200

    Reverse Account Menu If Statement

--- app/components/AccountMenu.tsx
+++ app/components/AccountMenu.tsx
@@ -11,56 +11,54 @@ interface AccountMenuProps {
 }
 
 export default function AccountMenu({ session }: AccountMenuProps) {
+  if (!session) {
+    return <LoginButton />;
+  }
+
   return (
-    <>
-      {session ? (
-        <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <Menu.Button className="text-white hover:text-background">
-              Sup! {session.user?.name}
-            </Menu.Button>
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="text-white hover:text-background">
+          Sup! {session.user?.name}
+        </Menu.Button>
+      </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-secondary rounded-md bg-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="px-1 py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  className={`${
+                    active ? 'bg-primary text-white' : 'text-primary'
+                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                >
+                  Profile
+                </button>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={() => signOut()}
+                  className={`${
+                    active ? 'bg-primary text-white' : 'text-primary'
+                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                >
+                  Sign out
+                </button>
+              )}
+            </Menu.Item>
           </div>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-secondary rounded-md bg-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="px-1 py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? 'bg-primary text-white' : 'text-primary'
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                      Profile
-                    </button>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      onClick={() => signOut()}
-                      className={`${
-                        active ? 'bg-primary text-white' : 'text-primary'
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                      Sign out
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
-      ) : (
-        <LoginButton />
-      )}
-    </>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
